79addef13f5bf2815eaa594c3f6ab088410190dd
nikhila086 <nikhilap086@gmail.com>
2025-04-04 21:49:30 +0530

    Add config.env to .gitignore

--- Frontend/src/components/NotFound/NotFound.jsx
+++ Frontend/src/components/NotFound/NotFound.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <>
+    <section className='page not found'>
+      <div className='content'>
+        <img src="/notfound.png" alt="Not Found" />
+        <Link to={"/"}>RETURN TO HOME</Link>
+      </div>
     
-    </>
+    </section>
   )
 }
 
