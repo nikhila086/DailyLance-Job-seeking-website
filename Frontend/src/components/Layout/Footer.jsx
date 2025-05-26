5ff3a8e8a645af15c7abd7485d5bdefe62995498
nikhila086 <nikhilap086@gmail.com>
2025-05-26 20:32:34 +0530

    changes in frontend

--- Frontend/src/components/Layout/Footer.jsx
+++ Frontend/src/components/Layout/Footer.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Context } from "../../main";
+import { Link } from "react-router-dom";
+import { FaFacebookF, FaYoutube, FaLinkedin , FaGithub} from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
 
 const Footer = () => {
+  const { isAuthorized } = useContext(Context);
   return (
-    <>
-    
-    </>
-  )
-}
+    <footer className={isAuthorized ? "footerShow" : "footerHide"}>
+      <div>&copy; All Rights Reserved By Nikhila Pediredla.</div>
+      <div>
+        
+        <Link to={"https://www.linkedin.com/in/nikhila-pediredla-14387926b"} target="_blank">
+          <FaLinkedin />
+        </Link>
 
-export default Footer
+        <Link to={"https://github.com/nikhila086"} target="_blank">
+          <FaGithub />
+        </Link>
+        
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- Frontend/src/components/NotFound/NotFound.jsx
+++ Frontend/src/components/NotFound/NotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <section className='page not found'>
+    <section className='page notfound'>
       <div className='content'>
         <img src="/notfound.png" alt="Not Found" />
         <Link to={"/"}>RETURN TO HOME</Link>
