86ee96d599187898be1afc7c32996d3f94951647
nikhila086 <nikhilap086@gmail.com>
2025-05-31 22:44:13 +0530

    last commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
   useEffect(() => {
     const fetchUser = async() =>{
       try {
-        const response = await axios.get("http://localhost:4000/api/v1/user/getuser", {withCredentials: true })
+        const response = await axios.get(`${import.meta.env.VITE_SERVER}/api/v1/user/getuser`, { withCredentials: true })
         setUser(response.data.user)
         setIsAuthorized(true)
       } catch (error) {
