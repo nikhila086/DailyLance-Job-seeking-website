0a7d681b64334145b5746c50c29a7d8c12a8a913
nikhila086 <nikhilap086@gmail.com>
2025-06-01 14:04:18 +0530

    Modified jwtToken.js

--- Backend/utils/jwtToken.js
+++ Backend/utils/jwtToken.js
@@ -5,6 +5,8 @@ export const sendToken = (user, statusCode, res, message) => {
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
       httpOnly: true, // Set httpOnly to true
+      secure: true, // Required on Render/Vercel (HTTPS)
+      sameSite: "None", // Must be 'None' for cross-origin
     };
   
     res.status(statusCode).cookie("token", token, options).json({
