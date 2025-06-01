b9f4160c614b344f7f9fd8e7500fa5e01ab175e7
nikhila086 <nikhilap086@gmail.com>
2025-06-01 11:27:38 +0530

    Modified app.js

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ dotenv.config({ path: "./config/config.env" });
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "DELETE", "PUT"],
+    method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
 );
